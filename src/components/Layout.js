675fbc0f4cb8cb6b95baae898b7dcefa906c5077
James Huskey <james@jameshuskey.com>
2018-10-30 02:02:29 -0500

    More commeit testing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | The Raging Jim Experience" />
     <Navbar />
     <div>{children}</div>
   </div>
