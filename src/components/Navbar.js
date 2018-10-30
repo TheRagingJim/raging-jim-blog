04b7407b568ce4f51ed01bd76658433e7f78f450
James Huskey <james@jameshuskey.com>
2018-10-30 08:10:42 -0500

    pushing additional  changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/tjrlogo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -9,17 +9,23 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="tjrlogo" style={{ width: '300px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/music">
+          Music
+        </Link>
+        <Link className="navbar-item" to="/gaming">
+          Gaming
+        </Link>
+        <Link className="navbar-item" to="/podcast">
+          Podcast
+        </Link>
         <Link className="navbar-item" to="/about">
           About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
+        </Link>        
       </div>
       <div className="navbar-end">
         <a
